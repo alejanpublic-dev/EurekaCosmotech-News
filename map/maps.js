d5f6a9bc8add8a41d815edc5ed649a8183efae9c
Manuel Garcia <alejanpublic.dev@gmail.com>
2024-01-04 23:32:59 -0300

    agregando dir del proyecto con apis de maps

---
+++ map/maps.js
@@ -0,0 +1,9 @@
+function iniciarmap(){
+    var coor = {lat:-34.6036844 ,lng: -58.3815591};
+    var map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 10,
+        center: coord
+    });
+}
+
+
